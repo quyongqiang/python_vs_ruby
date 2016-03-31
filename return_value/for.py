2a170e04522c3b5bd4abf5ae692956031ee85417
Larry <Chaniel@users.noreply.github.com>
2016-04-01 07:56:29 +0800

    Create for.py

---
+++ return_value/for.py
@@ -0,0 +1,21 @@
+prod1 = {"title":"Shirt, Men's", "size": 'XL', "cost":21}
+prod2 = {"title": "Skirt, Women's", "size": 'S', "cost":16}
+prod3 = {"title":"T-Shirt", "size":'M', "cost":14}
+myproducts = [prod1, prod2, prod3]
+
+
+
+#print myproducts
+
+def items_with_cost_greater_than(products, min_cost):
+  for i in products:
+  	pass
+
+
+
+
+print items_with_cost_greater_than(myproducts, 17)
+
+
+
+
